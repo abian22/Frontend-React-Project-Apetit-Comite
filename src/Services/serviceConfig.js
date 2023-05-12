2cf042b0fb8f35a1ebae806b0c61cb93ed85fd78
abian22 <abian.camejo@gmail.com>
2023-05-12 17:13:37 +0100

    baseURL modified

--- src/Services/serviceConfig.js
+++ src/Services/serviceConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://appetitcomite.onrender.com/api'
+    baseURL:' https://real-backend-production.up.railway.app/'
+    //'https://appetitcomite.onrender.com/api'
     // withCredentials: false,
     // headers: {
     //     Accept: 'application/json',
