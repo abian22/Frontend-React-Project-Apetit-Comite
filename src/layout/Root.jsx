dc262809c44ba943f17fd5a36e7e5d922755eadb
Abián <abian.camejo@gmail.com>
2025-01-15 17:06:26 +0000

    netlify.toml added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,13 +1,11 @@
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer/Footer";
 
 export default function Root() {
   return (
     <div>
       <Header />
       <Outlet />
-      <Footer />
     </div>
   );
 }
